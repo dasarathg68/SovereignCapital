1a0cce08597c97cb84650746586312e328bb124f
Dasarath G <dasarathg68@gmail.com>
2024-08-25 03:04:09 +0530

    feat: setup frontend

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -11,7 +11,18 @@ function App() {
   useEffect(() => {}, []);
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <ConnectButton />
+      <div className="flex flex-col gap-4 mt-20">
+        {isConnected ? (
+          <></>
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-4">
+            <h1 className="text-xl font-bold text-center">
+              Please connect your wallet to continue
+            </h1>
+            <ConnectButton />
+          </div>
+        )}{" "}
+      </div>
     </div>
   );
 }
---
+++ app/src/app/utils/constants.ts
@@ -0,0 +1,3 @@
+export const address = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "";
+export const chain = process.env.NEXT_PUBLIC_CHAIN || "";
+export const abi = [];
